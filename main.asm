1749e53f0f5b86caa5d8c1d6902760f3de19ba92
Toth Richard <toth.richard@lapcom.hu>
2018-04-08 18:36:20 +0200

    unicode char is ok

--- main.asm
+++ main.asm
@@ -7,6 +7,10 @@ section .data
 	bye DB "Program vege", 0xA
 	l_bye equ $ - bye
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Here is the sequence chars to graphic ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 	pos DB 27, "[10;15H"
 	pos_l equ $ - pos
 
@@ -18,6 +22,13 @@ section .data
 
 	show_cur DB 27, "[?25h"
 	show_cur_l equ $ - show_cur
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; The box drawing chars to window ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+	t_l_cor DB `\u250C`
+	t_l_len equ $ - t_l_cor
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -38,7 +49,7 @@ write_ erase_disp, erase_l
 loop_:
 
 	write_ pos, pos_l
-	write_ msg, len
+	write_ t_l_cor, t_l_len
 
 	read_ usr_in, 1
 
